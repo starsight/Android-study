f56b3dea3618d634cb642e8332152fd580c9018d
hewenjie <172583172@qq.com>
2016-09-15 14:15:09 +0800

    9-15 绘画firstview grasture draw

--- app/src/main/java/com/wenjiehe/android_study/view/FirstView.java
+++ app/src/main/java/com/wenjiehe/android_study/view/FirstView.java
@@ -1,10 +1,13 @@
 package com.wenjiehe.android_study.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.wenjiehe.android_study.R;
@@ -13,7 +16,13 @@ import com.wenjiehe.android_study.R;
  * Created by yiyuan on 2016/9/14.
  */
 public class FirstView extends View {
-    private Paint mypaint;
+    private Paint mPaint;  //绘制线条的Path
+    private Path mPath;      //记录用户绘制的Path
+    private Canvas mCanvas;  //内存中创建的Canvas
+    private Bitmap mBitmap;  //缓存绘制的内容
+
+    private int mLastX;
+    private int mLastY;
 
     public FirstView(Context context) {
         super(context);
@@ -30,24 +39,65 @@ public class FirstView extends View {
         init();
     }
 
-    private void init (){
-        mypaint = new Paint();
-        mypaint.setAntiAlias(true);
-        mypaint.setColor(getResources().getColor(android.R.color.darker_gray));
-        mypaint.setStyle(Paint.Style.STROKE);
-        mypaint.setStrokeWidth(4);
+    private void init(){
+        mPath = new Path();
+        mPaint = new Paint();   //初始化画笔
+        mPaint.setColor(Color.GREEN);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND); //结合处为圆角
+        mPaint.setStrokeCap(Paint.Cap.ROUND); // 设置转弯处为圆角
+        mPaint.setStrokeWidth(20);   // 设置画笔宽度
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawColor(getResources().getColor(R.color.colorPrimary));
-        Path path = new Path();
-        path.moveTo(200,100);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+        // 初始化bitmap,Canvas
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
 
-        path.lineTo(100,210);
-        path.lineTo(300,270);
-        //path.rCubicTo(200,100,250,200,500,350);
-        canvas.drawPath(path,mypaint);
+    //重写该方法，在这里绘图
+    @Override
+    protected void onDraw(Canvas canvas) {
+        drawPath();
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+    }
+
+    //绘制线条
+    private void drawPath(){
+        mCanvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = event.getAction();
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = x;
+                mLastY = y;
+                mPath.moveTo(mLastX, mLastY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int dx = Math.abs(x - mLastX);
+                int dy = Math.abs(y - mLastY);
+                if (dx > 3 || dy > 3)
+                    mPath.lineTo(x, y);
+                mLastX = x;
+                mLastY = y;
+                break;
+        }
+
+        invalidate();
+        return true;
     }
 }
