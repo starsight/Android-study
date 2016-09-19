a106b2f8eb8aaf366640677cc943b505dfdd0bd3
starsight <172583172@qq.com>
2016-09-19 19:36:55 +0800

    9-19  dragonborad初始化

---
+++ app/src/main/java/com/wenjiehe/android_study/dragonboard/GetImageActivity.java
@@ -0,0 +1,15 @@
+package com.wenjiehe.android_study.dragonboard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wenjiehe.android_study.R;
+
+public class GetImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_image);
+    }
+}
